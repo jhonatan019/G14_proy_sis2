a816d454f14bf35b22746bd5b505436b8a2d2356
jhonatan <jmamani.m.00@gmail.com>
2025-01-27 02:23:18 +0000

    prueba 2

--- Cinecito2.java
+++ Cinecito2.java
@@ -11,6 +11,6 @@ package com.mycompany.cinecito2;
 public class Cinecito2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
